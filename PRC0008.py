9719a5c3cec0de70a7649295da70dfc45cc6bdb3
Lukas Robin <lukasmrobin@gmail.com>
2021-08-17 01:49:35 -0400

    semi-finalised product

--- PRC0008.py
+++ PRC0008.py
@@ -4,20 +4,15 @@ def GET_ROW_COUNT() -> int:
         battleRoyaleData = csv.reader(source, delimiter=',')
         row_count = sum(1 for row in battleRoyaleData)
     return row_count
+
 def DISPLAY_PLAYERS() -> tuple:
     data = []
     with open('battle_royale.csv', 'r') as source:
-        source.readline()
-        for row in source:
-        fields = row.strip().split(",") 
-        data.append({
-        'Avatar Name' : fields[0],
-        'Name'  : fields[1],
-        'Number'   : int(fields[2])
-    })
-    return data
+        battleRoyaleData = csv.reader(source, delimiter=',')
+        for row in battleRoyaleData:
+            print(row)
 
-def WRITE_PLAYER(avatarName, name) -> None:
+def WRITE_PLAYER(avatarName: str, name: str) -> None:
     csv_list = []
     rowCount = GET_ROW_COUNT()
     with open('battle_royale.csv', 'r') as source:
@@ -28,32 +23,44 @@ def WRITE_PLAYER(avatarName, name) -> None:
     with open('battle_royale.csv', 'w', newline='') as csvfile:
         newWrite = csv.writer(csvfile, delimiter=',')
         newWrite.writerows(csv_list)
-def IS_REGISTERED(avatarName) -> bool:
+
+def IS_REGISTERED(avatarName: str):
     csv_list = []
     with open('battle_royale.csv', 'r') as source:
         battleRoyaleData = csv.reader(source, delimiter=',')
         for row in battleRoyaleData:
             csv_list.append(row)
-        if not any(avatarName in x for x in csv_list):
-           return True
-        else:
-            return False
-def main() -> None:
+        return any(avatarName in x for x in csv_list)
+           
+
+def main():
     wouldLikeToCheckIfRegistered = input("Would you like to check if your avatar name is registered? \n")
-        if wouldLikeToCheckIfRegistered.lower() == 'yes' or wouldLikeToCheckIfRegistered.lower() == 'y':
-            avatarName = input("Enter avatar name. \n")
-            if IS_REGISTERED():
-                print("You are already registered. \n")
-            else:
-                print("You are not registered, would you like to register under that avatar name? ")
-                wouldLikeToRegister = input("\n")
-                if wouldLikeToRegister.lower() == 'y' or wouldLikeToRegister.lower() == 'yes':
-                    name = input("Enter your name.\n")
-                    WRITE_PLAYER(avatarName, name)
+    if wouldLikeToCheckIfRegistered.lower() == 'yes' or wouldLikeToCheckIfRegistered.lower() == 'y':
+        avatarName = input("Enter avatar name. \n")
+        if IS_REGISTERED(avatarName) == True:
+            print("You are already registered. ")
         else:
-            wouldLikeList = input("Would you like a list of participants? ")
-            if wouldLikeList.lower() == "y" or wouldLikeList.lower() == 'yes':
-                DISPLAY_PLAYERS()
+            print("You are not registered, would you like to register under that avatar name? ")
+            wouldLikeToRegister = input("")
+            if wouldLikeToRegister.lower() == 'y' or wouldLikeToRegister.lower() == 'yes':
+                name = input("Enter your name.\n")
+                WRITE_PLAYER(avatarName, name)
+    else:
+        wouldLikeList = input("Would you like a list of participants? ")
+        wantLikeList = lambda wouldLikeList : wouldLikeList.lower() == "y" or wouldLikeList.lower() == 'yes'
+        if wantLikeList:
+            DISPLAY_PLAYERS()
+            exit()
+        else:
+            wouldLikeToRegister = input("Would you like to register? \n")
+            if wouldLikeToRegister.lower() == 'y' or wouldLikeToRegister.lower() == 'yes':
+                avatarName = input("Enter avatar name.\n")
+                name = input("Enter name.\n")
+                WRITE_PLAYER(avatarName, name)
+                exit()
+            else:
+                exit()
+
 if __name__ == "__main__":
     main()
 else:
