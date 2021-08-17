f2638630b7a7e50574be8aa6f60e56536d8b90b4
Lukas Robin <lukasmrobin@gmail.com>
2021-08-17 00:50:48 -0400

    update

--- PRC0008.py
+++ PRC0008.py
@@ -4,12 +4,20 @@ def GET_ROW_COUNT() -> int:
         battleRoyaleData = csv.reader(source, delimiter=',')
         row_count = sum(1 for row in battleRoyaleData)
     return row_count
-def DISPLAY_PLAYERS():
+def DISPLAY_PLAYERS() -> tuple:
+    data = []
     with open('battle_royale.csv', 'r') as source:
-        battleRoyaleData = csv.reader(source, delimiter=',')
-        for row in battleRoyaleData:
-            print(row)
-def WRITE_PLAYER(avatarName, name):
+        source.readline()
+        for row in source:
+        fields = row.strip().split(",") 
+        data.append({
+        'Avatar Name' : fields[0],
+        'Name'  : fields[1],
+        'Number'   : int(fields[2])
+    })
+    return data
+
+def WRITE_PLAYER(avatarName, name) -> None:
     csv_list = []
     rowCount = GET_ROW_COUNT()
     with open('battle_royale.csv', 'r') as source:
@@ -20,7 +28,7 @@ def WRITE_PLAYER(avatarName, name):
     with open('battle_royale.csv', 'w', newline='') as csvfile:
         newWrite = csv.writer(csvfile, delimiter=',')
         newWrite.writerows(csv_list)
-def IS_REGISTERED(avatarName):
+def IS_REGISTERED(avatarName) -> bool:
     csv_list = []
     with open('battle_royale.csv', 'r') as source:
         battleRoyaleData = csv.reader(source, delimiter=',')
@@ -30,7 +38,7 @@ def IS_REGISTERED(avatarName):
            return True
         else:
             return False
-def main():
+def main() -> None:
     wouldLikeToCheckIfRegistered = input("Would you like to check if your avatar name is registered? \n")
         if wouldLikeToCheckIfRegistered.lower() == 'yes' or wouldLikeToCheckIfRegistered.lower() == 'y':
             avatarName = input("Enter avatar name. \n")
@@ -42,6 +50,10 @@ def main():
                 if wouldLikeToRegister.lower() == 'y' or wouldLikeToRegister.lower() == 'yes':
                     name = input("Enter your name.\n")
                     WRITE_PLAYER(avatarName, name)
+        else:
+            wouldLikeList = input("Would you like a list of participants? ")
+            if wouldLikeList.lower() == "y" or wouldLikeList.lower() == 'yes':
+                DISPLAY_PLAYERS()
 if __name__ == "__main__":
     main()
 else:
